5262c391b0905e8defc11e013cabc4f1d0d42fcd
Mihail <terem848@gmail.com>
2025-08-27 07:57:31 +0300

    review 11.js

--- 01/11.js
+++ 01/11.js
@@ -1,104 +1,97 @@
 // Реализуй ограничение параллелизма при выполнении массива async-функций (не больше 2-х одновременно)
 
-function asyncBarier(promises, times) {
-  const map = new Map();
+function runLimitedConcurrency(promises, times) {
+  const promisesMap = new Map();
   for (let i = 0; i < promises.length; i++) {
-    map.set(promises[i], null);
+    promisesMap.set(promises[i], null);
   }
-  const indexes = [];
+  const execPromiseOnOneTimeIndexes = [];
   for (let i = 0; i < times; i++) {
-    indexes.push(i);
+    execPromiseOnOneTimeIndexes.push(i);
   }
 
-  runPromise(promises, indexes, map);
+  runPromise(promisesMap, times);
 }
 
-function runPromise(promises, indexies, map) {
-  const indxs = Array.from(indexies);
-
-  const arr = [];
-  for (let i = 0; i < indexies.length; i++) {
-    if (map.get(promises[indexies[i]]) === null) {
-      arr.push(promises[indexies[i]]);
-      map.set(promises[indexies[i]], "do");
+function runPromise(map, times) {
+  const executablePromises = [];
+  const dontExecutablePromises = [];
+  for (const [promise, status] of map) {
+    if (status === "do") {
+      executablePromises.push(promise);
+    }
+    if (status === null) {
+      dontExecutablePromises.push(promise);
     }
   }
-  for (let i = 0; i < arr.length; i++) {
-    arr[i]()
+
+  if (dontExecutablePromises.length == 0) {
+    return;
+  }
+
+  for (let i = 0; i < times - executablePromises.length; i++) {
+    map.set(dontExecutablePromises[i], "do");
+    dontExecutablePromises[i]()
       .then((res) => console.log(res))
       .finally(() => {
-        map.set(arr[i], "done");
-        promiseLoop: for (let j = 0; j < promises.length; j++) {
-          if (promises[j] === arr[i]) {
-            for (let m = 0; m < indexies.length; m++) {
-              if (indexies[m] === j) {
-                for (let n = 0; n < promises.length; n++) {
-                  if (map.get(promises[n]) === null) {
-                    indxs[m] = n;
-                    break promiseLoop;
-                  }
-                }
-              }
-            }
-          }
-        }
-        runPromise(promises, indxs, map);
+        map.set(dontExecutablePromises[i], "done");
+        runPromise(map, times);
       });
   }
 }
 
-asyncBarier(
-  [
-    () =>
-      new Promise((resolve) => {
-        console.log("work 1 promise");
-        setTimeout(() => resolve("1 promise is done"), 1000);
-      }),
-    () =>
-      new Promise((resolve) => {
-        console.log("work 2 promise");
-        setTimeout(() => resolve("2 promise is done"), 1001);
-      }),
-    () =>
-      new Promise((resolve) => {
-        console.log("work 3 promise");
-        setTimeout(() => resolve("3 promise is done"), 1002);
-      }),
-    () =>
-      new Promise((resolve) => {
-        console.log("work 4 promise");
-        setTimeout(() => resolve("4 promise is done"), 1003);
-      }),
-    () =>
-      new Promise((resolve) => {
-        console.log("work 5 promise");
-        setTimeout(() => resolve("5 promise is done"), 1004);
-      }),
-    () =>
-      new Promise((resolve) => {
-        console.log("work 6 promise");
-        setTimeout(() => resolve("6 promise is done"), 1005);
-      }),
-    () =>
-      new Promise((resolve) => {
-        console.log("work 7 promise");
-        setTimeout(() => resolve("7 promise is done"), 1006);
-      }),
-    () =>
-      new Promise((resolve) => {
-        console.log("work 8 promise");
-        setTimeout(() => resolve("8 promise is done"), 1007);
-      }),
-    () =>
-      new Promise((resolve) => {
-        console.log("work 9 promise");
-        setTimeout(() => resolve("9 promise is done"), 1008);
-      }),
-    () =>
-      new Promise((resolve) => {
-        console.log("work 10 promise");
-        setTimeout(() => resolve("10 promise is done"), 1009);
-      }),
-  ],
-  3
-);
+const tasks = [
+  () =>
+    new Promise((resolve) => {
+      console.log("work 1 promise");
+      setTimeout(() => resolve("1 promise is done"), 1000);
+    }),
+  () =>
+    new Promise((resolve) => {
+      console.log("work 2 promise");
+      setTimeout(() => resolve("2 promise is done"), 1001);
+    }),
+  () =>
+    new Promise((resolve) => {
+      console.log("work 3 promise");
+      setTimeout(() => resolve("3 promise is done"), 1002);
+    }),
+  () =>
+    new Promise((resolve) => {
+      console.log("work 4 promise");
+      setTimeout(() => resolve("4 promise is done"), 1003);
+    }),
+  () =>
+    new Promise((resolve) => {
+      console.log("work 5 promise");
+      setTimeout(() => resolve("5 promise is done"), 1004);
+    }),
+  () =>
+    new Promise((resolve) => {
+      console.log("work 6 promise");
+      setTimeout(() => resolve("6 promise is done"), 1005);
+    }),
+  () =>
+    new Promise((resolve) => {
+      console.log("work 7 promise");
+      setTimeout(() => resolve("7 promise is done"), 1006);
+    }),
+  () =>
+    new Promise((resolve) => {
+      console.log("work 8 promise");
+      setTimeout(() => resolve("8 promise is done"), 1007);
+    }),
+  () =>
+    new Promise((resolve) => {
+      console.log("work 9 promise");
+      setTimeout(() => resolve("9 promise is done"), 1008);
+    }),
+  () =>
+    new Promise((resolve) => {
+      console.log("work 10 promise");
+      setTimeout(() => resolve("10 promise is done"), 1009);
+    }),
+];
+
+runLimitedConcurrency(tasks, 5);
+
